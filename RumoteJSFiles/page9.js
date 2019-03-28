a4fd1dcd955d64f010e4ed180d1e0cf581f80bae
JRAllum <allum434@gmail.com>
2019-03-28 12:00:04 +0100

    Laget JS file. Oppdatert CSS

---
+++ RumoteJSFiles/page9.js
@@ -0,0 +1,11 @@
+function page9() {
+    document.getElementById('content').innerHTML =
+    `
+              <div id="side9">
+                  <h2>How can we improve?</h2>
+                  <textarea type="text"></textarea>
+                  <button type="button" class="btn btn-primary btn-sm bottomright">Continue</button>
+              </div>
+    `;
+}
+         
