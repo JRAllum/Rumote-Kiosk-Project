dc33938f6fb34504d74b517ff8d5b963a1e20bed
JRAllum <allum434@gmail.com>
2019-04-02 12:36:08 +0200

    Oppdateringer og js-filer

---
+++ RumoteJSFiles/page11.js
@@ -0,0 +1,54 @@
+function page11() {
+    document.getElementById("mainContent").innerHTML = `
+    <div id="side11"></div>
+    <h2>Please, confirm your order.</h2>
+    <table>
+      <tr>
+        <td>Four bedroom apartments:</td>
+        <td></td>
+        <td></td>
+        <td>1000kr</td>
+      </tr>
+      <tr>
+        <td>torsdag, 07.03. - fredag, 08.03</td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>Bed linen 4 sets</td>
+        <td></td>
+        <td></td>
+        <td>1000kr</td>
+      </tr>
+      <tr>
+        <td>cleaning</td>
+        <td></td>
+        <td></td>
+        <td>1000kr</td>
+      </tr>
+
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td>----------</td>
+      </tr>
+      
+
+
+      <tr>
+        <td></td>
+        <td></td>
+        <td>Total sum:</td>
+        <td>3000kr</td>
+      </tr>
+
+
+    </table>
+
+    <div class="text-center">
+      <button type="button" class="btn btn-primary bottomright">Primary</button>
+    </div>
+
+  </div>`
---
+++ RumoteJSFiles/page6.js
@@ -0,0 +1,118 @@
+function page5() {
+    document.getElementById("mainContent").innerHTML = `
+    <div class="page6">
+    <div class="info">
+        <p>Card number for testing purposes is:<br>
+           4242 4242 4242 4242.<br>
+          Everything else is random.</p>
+    </div>
+  <script src="https://js.stripe.com/v3/"></script>
+  <form>
+    <label>
+      <input
+        name="cardholder-name"
+        class="field is-empty"
+        placeholder="Full name"
+      />
+      <span><span>Name</span></span>
+    </label>
+    <label>
+      <input
+        class="field is-empty"
+        type="tel"
+        placeholder="xx xxx xxx"
+      />
+      <span><span>Phone number</span></span>
+    </label>
+    <label>
+      <div id="card-element" class="field is-empty"></div>
+      <span><span>Credit or debit card</span></span>
+    </label>
+    <button type="submit">Pay $25</button>
+    <div class="outcome">
+      <div class="error" role="alert"></div>
+      <div class="success">
+        Success! Your Stripe token is <span class="token"></span>
+      </div>
+    </div>
+  </form>
+  <script>
+      var stripe = Stripe("pk_test_c1AMZEnN0unLBDXAr9nFo7xG00p6gwDTuQ");
+      var elements = stripe.elements();
+
+      var card = elements.create("card", {
+        iconStyle: "solid",
+        style: {
+          base: {
+            iconColor: "#8898AA",
+            color: "black",
+            lineHeight: "36px",
+            fontWeight: 300,
+            fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+            fontSize: "19px",
+
+            "::placeholder": {
+              color: "#8898AA"
+            }
+          },
+          invalid: {
+            iconColor: "#e85746",
+            color: "#e85746"
+          }
+        },
+        classes: {
+          focus: "is-focused",
+          empty: "is-empty"
+        }
+      });
+      card.mount("#card-element");
+
+      var inputs = document.querySelectorAll("input.field");
+      Array.prototype.forEach.call(inputs, function(input) {
+        input.addEventListener("focus", function() {
+          input.classList.add("is-focused");
+        });
+        input.addEventListener("blur", function() {
+          input.classList.remove("is-focused");
+        });
+        input.addEventListener("keyup", function() {
+          if (input.value.length === 0) {
+            input.classList.add("is-empty");
+          } else {
+            input.classList.remove("is-empty");
+          }
+        });
+      });
+
+      function setOutcome(result) {
+        var successElement = document.querySelector(".success");
+        var errorElement = document.querySelector(".error");
+        successElement.classList.remove("visible");
+        errorElement.classList.remove("visible");
+
+        if (result.token) {
+          // Use the token to create a charge or a customer
+          // https://stripe.com/docs/charges
+          successElement.querySelector(".token").textContent = result.token.id;
+          successElement.classList.add("visible");
+        } else if (result.error) {
+          errorElement.textContent = result.error.message;
+          errorElement.classList.add("visible");
+        }
+      }
+
+      card.on("change", function(event) {
+        setOutcome(event);
+      });
+
+      document.querySelector("form").addEventListener("submit", function(e) {
+        e.preventDefault();
+        var form = document.querySelector("form");
+        var extraDetails = {
+          name: form.querySelector("input[name=cardholder-name]").value
+        };
+        stripe.createToken(card, extraDetails).then(setOutcome);
+      });
+    </script>
+</div>`;
+    }
