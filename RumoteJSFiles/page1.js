6e3157936c324bf9eeb206db95f09a6ed0121872
Olga <olga@getacademy.no>
2019-04-22 21:52:10 +0200

    Changed the grid a bit

--- RumoteJSFiles/page1.js
+++ RumoteJSFiles/page1.js
@@ -1,7 +1,8 @@
 function page1() {
 	document.getElementById('mainContent').innerHTML = `
-    <h2> Welcome to Alpin Lodge</h2> <br>
-    <button onclick= "page2()"  type="button" class="btn btn-primary btn-lg buttonCheckInOutSize"> Check in </button>
-    <button onclick= "page11()"   type="button" class="btn btn-primary btn-lg buttonCheckInOutSize">Check out </button>
-    `;
+    <h2> Welcome to Alpin Lodge</h2> <br><br><br>
+    <div>
+    <button onclick= "page2()"  type="button" class="btn btn-primary btn-lg buttonCheckInOutSize btns "> Check in </button>
+    <button onclick= "page11()"   type="button" class="btn btn-primary btn-lg  buttonCheckInOutSize btns" > Check out </button>
+    </div>`;
 }
