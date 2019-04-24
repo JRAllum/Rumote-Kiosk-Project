b7bc3d9e629bf836f617e66a70a8a5caa6d64514
JRAllum <allum434@gmail.com>
2019-04-24 11:01:34 +0200

    small updates

--- RumoteJSFiles/page9.js
+++ RumoteJSFiles/page9.js
@@ -2,7 +2,7 @@ function page9() {
   document.getElementById("mainContent").innerHTML = `
               <div id="side9">
                   <h2>How can we improve?</h2>
-                  <textarea class="page9Text" type="text"></textarea>
+                  <textarea type="text"></textarea>
                   <button type="button" class="btn btn-primary btn-sm bottomright">Continue</button>
               </div>
     `;
