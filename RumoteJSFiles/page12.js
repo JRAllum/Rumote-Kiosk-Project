d0f330bb2ea03e96fbcf17c0fc0c1386f8e45c58
JRAllum <allum434@gmail.com>
2019-05-08 11:14:42 +0200

    gt

--- RumoteJSFiles/page12.js
+++ RumoteJSFiles/page12.js
@@ -2,7 +2,7 @@ function page12() {
   document.getElementById("mainContent").innerHTML = `
   <div id="page12">
         <div id="masterlock">
-        <img src="videos/master.gif" type="video/move" autoplay="" loop="">
+        <img src="master.gif" type="video/move" autoplay="" loop="">
         </img>
     </div>
               `;
